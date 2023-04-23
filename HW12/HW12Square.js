eb24d69490ae47836d414e9dcbec6cc766eb26c1
Cole Yeager <106110474+ColeYeager19@users.noreply.github.com>
2023-04-23 17:32:07 -0600

    Update HW12Square.js

--- HW12/HW12Square.js
+++ HW12/HW12Square.js
@@ -43,3 +43,47 @@ class Square{
         return this.color;
     }
 }
+
+class Collectibles{
+
+    constructor(xCoord,yCoord,objectHeight,objectWidth, color)
+    {
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
+        this.objectHeight = objectHeight;
+        this.objectWidth = objectWidth;
+        this.color = color;
+    }
+
+    get x()
+    {
+        return this.xCoord;
+    }
+
+    set x(value)
+    {
+        this.xCoord = value;
+    }
+    get y()
+    {
+        return this.yCoord;
+    }
+    set y(value)
+    {
+        this.yCoord = value;
+    }
+
+    get height()
+    {
+        return this.objectHeight;
+    }
+
+    get width()
+    {
+        return this.objectWidth;
+    }
+    get mainColor()
+    {
+        return this.color;
+    }
+}
